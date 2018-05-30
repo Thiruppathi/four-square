11558e9308304c77f01cd53b384f8073a647e79c
thiru <thiruppathi.muthukumar@ee.co.uk>
2018-05-30 15:50:52 +0100

    ♻️ Refactoring & 💄 Beautification

--- public/javascripts/four-square-app.js
+++ public/javascripts/four-square-app.js
@@ -4,5 +4,4 @@ import "../sass/style.scss";
 import { $, $$ } from "./modules/bling";
 import autocomplete from "./modules/autocomplete";
 
-autocomplete($("#address"), $("#lat"), $("#lng"));
-autocomplete($("#near"), $("#fromLat"), $("#fromLng"));
+autocomplete($("#near"), $("#lat"), $("#lng"));
